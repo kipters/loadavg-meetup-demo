151844af2d57d7dfa7426679ff4f3de922397a7d
Fabio Di Peri <fabiodiperi@outlook.com>
2019-12-19 16:02:05 +0000

    Redis pub as json

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import sqlite3
 import redis
+import json
 from time import sleep
 from os import getloadavg
 from atexit import register
+from socket import gethostname
+from datetime import datetime
 
+hn = gethostname()
 db = sqlite3.connect('avg.db')
 r = redis.Redis('redis')
 
@@ -16,7 +20,16 @@ db.execute('CREATE TABLE IF NOT EXISTS load (ts INTEGER NOT NULL, avg NUMBER NOT
 
 while True:
     avg = getloadavg()
-    r.publish('avg', str(avg[1]))
+
+    data = {
+        'ts': datetime.now().isoformat(),
+        'host': hn,
+        'avg': str(avg[1])
+    }
+
+    msg = json.dumps(data)
+
+    r.publish('avg', msg)
     db.execute("INSERT INTO load (ts, avg) VALUES (datetime('now'), ?)", (avg[1],))
     db.commit()
     print ("Load avg 5s: " + str(avg[1]))
