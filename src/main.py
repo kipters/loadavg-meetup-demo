e0d1e03fc9b0301c9cc6b9329a7bd965d422c2a2
Fabio Di Peri <fabiodiperi@outlook.com>
2019-12-19 15:57:42 +0000

    Redis pubsub

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import sqlite3
+import redis
 from time import sleep
 from os import getloadavg
 from atexit import register
 
 db = sqlite3.connect('avg.db')
+r = redis.Redis('redis')
 
 def closeDb():
     db.close()
@@ -14,7 +16,8 @@ db.execute('CREATE TABLE IF NOT EXISTS load (ts INTEGER NOT NULL, avg NUMBER NOT
 
 while True:
     avg = getloadavg()
+    r.publish('avg', str(avg[1]))
     db.execute("INSERT INTO load (ts, avg) VALUES (datetime('now'), ?)", (avg[1],))
     db.commit()
     print ("Load avg 5s: " + str(avg[1]))
-    sleep(5)
+    sleep(1)
